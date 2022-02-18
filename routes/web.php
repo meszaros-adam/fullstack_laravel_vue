8eb0f3d096bbc219fe458a472e68b82b9a623102
Mészáros Ádám <meszike91@gmail.com>
2022-02-18 20:41:25 +0100

    middleware for app routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\AdminCheck;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,20 +14,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('app/create_tag',[App\Http\Controllers\AdminController::class, 'addTag']);
-Route::get('app/get_tags',[App\Http\Controllers\AdminController::class, 'getTag']);
-Route::post('app/edit_tag',[App\Http\Controllers\AdminController::class, 'editTag']);
-Route::post('app/delete_tag',[App\Http\Controllers\AdminController::class, 'deleteTag']);
-Route::post('app/upload',[App\Http\Controllers\AdminController::class, 'upload']);
-Route::post('app/delete_image',[App\Http\Controllers\AdminController::class, 'deleteImage']);
-Route::post('app/create_category',[App\Http\Controllers\AdminController::class, 'addCategory']);
-Route::get('app/get_category',[App\Http\Controllers\AdminController::class, 'getCategory']);
-Route::post('app/edit_category',[App\Http\Controllers\AdminController::class, 'editCategory']);
-Route::post('app/delete_category',[App\Http\Controllers\AdminController::class, 'deleteCategory']);
-Route::post('app/create_user',[App\Http\Controllers\AdminController::class, 'addUser']);
-Route::get('app/get_users',[App\Http\Controllers\AdminController::class, 'getUser']);
-Route::post('app/edit_user',[App\Http\Controllers\AdminController::class, 'editUser']);
-Route::post('app/admin_login',[App\Http\Controllers\AdminController::class, 'adminLogin']);
+Route::prefix('app')->middleware([AdminCheck::class])->group(function(){
+    Route::post('/create_tag',[App\Http\Controllers\AdminController::class, 'addTag']);
+    Route::get('/get_tags',[App\Http\Controllers\AdminController::class, 'getTag']);
+    Route::post('/edit_tag',[App\Http\Controllers\AdminController::class, 'editTag']);
+    Route::post('/delete_tag',[App\Http\Controllers\AdminController::class, 'deleteTag']);
+    Route::post('/upload',[App\Http\Controllers\AdminController::class, 'upload']);
+    Route::post('/delete_image',[App\Http\Controllers\AdminController::class, 'deleteImage']);
+    Route::post('/create_category',[App\Http\Controllers\AdminController::class, 'addCategory']);
+    Route::get('/get_category',[App\Http\Controllers\AdminController::class, 'getCategory']);
+    Route::post('/edit_category',[App\Http\Controllers\AdminController::class, 'editCategory']);
+    Route::post('/delete_category',[App\Http\Controllers\AdminController::class, 'deleteCategory']);
+    Route::post('/create_user',[App\Http\Controllers\AdminController::class, 'addUser']);
+    Route::get('/get_users',[App\Http\Controllers\AdminController::class, 'getUser']);
+    Route::post('/edit_user',[App\Http\Controllers\AdminController::class, 'editUser']);
+    Route::post('/admin_login',[App\Http\Controllers\AdminController::class, 'adminLogin']);
+});
+
+
 
 Route::get('/logout',[App\Http\Controllers\AdminController::class, 'logout']);
 Route::get('/',[App\Http\Controllers\AdminController::class, 'index']);
